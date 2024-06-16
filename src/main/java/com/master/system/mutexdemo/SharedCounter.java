e84df536cc6f5afcb77f4d2def5e915b797588d9
Nitesh Nandan <niteshnitp5686@gmail.com>
2024-06-17 00:11:57 +0530

    Refactor as per review

--- src/main/java/com/master/system/mutexdemo/SharedCounter.java
+++ src/main/java/com/master/system/mutexdemo/SharedCounter.java
@@ -17,31 +17,4 @@ public class SharedCounter {
             counter = counter + 1;
         }
     }
-
-    public static void main(String[] args) throws InterruptedException {
-        SharedCounter counter = new SharedCounter();
-
-        int numThreads = 10;
-        int incrementsPerThread = 10;
-
-        Thread[] threads = new Thread[numThreads];
-
-        for (int i = 0; i < numThreads; i++) {
-            threads[i] = new Thread(() -> {
-                for (int j = 0; j < incrementsPerThread; j++) {
-                    counter.incrementWithMutex();
-                }
-            });
-        }
-
-        for (Thread thread : threads) {
-            thread.start();
-        }
-
-        for (Thread thread : threads) {
-            thread.join();
-        }
-
-        System.out.println("Final counter value: " + counter.getCounter());
-    }
 }
